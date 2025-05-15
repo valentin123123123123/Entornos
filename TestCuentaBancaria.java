edb53f670da9d4904c2d63dcf74613962b999c40
ValentinOcampo Practica Git <jop0007@alu.medac.es>
2025-05-15 22:42:35 +0200

    Cambio en master para provocar conflicto

--- TestCuentaBancaria.java
+++ TestCuentaBancaria.java
@@ -4,7 +4,7 @@ class TestCuentaBancaria
 	public static void main(String args[])
 	{
 		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100.0, 2.5);
-		CuentaBancaria cuenta2=new CuentaBancaria("11111111", "Valentin", "1000.0", "5.0");
+		CuentaBancaria cuenta2=new CuentaBancaria("11111111", "Valentinonflicto",100.0,3.0);
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
